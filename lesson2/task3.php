350f674f0ce7de40c0a3c5b76bee6dbdac9a576a
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2022-02-27 10:42:36 +0300

    Выполнил задание №3.

--- lesson2/task3.php
+++ lesson2/task3.php
@@ -1,1 +1,28 @@
 <?php
+function summ($a, $b)
+{
+    return $a + $b;
+}
+
+function subtraction($a, $b)
+{
+    return $a - $b;
+}
+
+function mul($a, $b)
+{
+    return $a * $b;
+}
+
+function div($a, $b)
+{
+    return $a / $b;
+}
+
+$a = 10;
+$b = 5;
+
+echo "summ($a, $b) = " . summ($a, $b) . PHP_EOL;
+echo "subtraction($a, $b) = " . subtraction($a, $b) . PHP_EOL;
+echo "mul($a, $b) = " . mul($a, $b) . PHP_EOL;
+echo "div($a, $b) = " . div($a, $b) . PHP_EOL;
