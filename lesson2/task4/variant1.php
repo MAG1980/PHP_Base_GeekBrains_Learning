f5efbea24e0ab940a86e97f7d002b972823ea750
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2022-02-27 18:16:08 +0300

    Выполнил задание №5. Параметры в функцию renderTemplate()  лучше передавать в виде массива.

--- lesson2/task4/variant1.php
+++ lesson2/task4/variant1.php
@@ -1,5 +1,5 @@
 <?php
-include "task3.php";
+include "../task3.php";
 
 $arg1 = 10;
 $arg2 = 5;
@@ -29,7 +29,7 @@ function mathOperation($arg1, $arg2, $operation)
         }
 
         default:
-            break;
+            return "Ошибка оператора!";
     }
 }
 
@@ -37,5 +37,7 @@ echo "mathOperation($arg1, $arg2, 'summ') = " . mathOperation($arg1, $arg2, "sum
 echo "mathOperation($arg1, $arg2, 'subtraction') = " . mathOperation($arg1, $arg2, "subtraction") . PHP_EOL;
 echo "mathOperation($arg1, $arg2, 'mul') = " . mathOperation($arg1, $arg2, "mul") . PHP_EOL;
 echo "mathOperation($arg1, $arg2, 'div') = " . mathOperation($arg1, $arg2, "div") . PHP_EOL;
+echo "mathOperation($arg1, $arg2, 'div') = " . mathOperation($arg1, 0, "div") . PHP_EOL;
+echo "mathOperation($arg1, $arg2, 'div') = " . mathOperation($arg1, $arg2, "iv") . PHP_EOL;
 
 
