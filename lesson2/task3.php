8b77671777f01edbfcbfacfb26e63f348981c0f0
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2022-02-27 11:16:34 +0300

    Выполнил задание №4. Исправил ошибку в решении задания №3: добавил проверку деления на 0.

--- lesson2/task3.php
+++ lesson2/task3.php
@@ -16,13 +16,15 @@ function mul($a, $b)
 
 function div($a, $b)
 {
+    if ($b === 0) return "Ошибка! Деление на 0 запрещено! Введите корректный делитель!";
     return $a / $b;
 }
 
 $a = 10;
 $b = 5;
 
-echo "summ($a, $b) = " . summ($a, $b) . PHP_EOL;
-echo "subtraction($a, $b) = " . subtraction($a, $b) . PHP_EOL;
-echo "mul($a, $b) = " . mul($a, $b) . PHP_EOL;
-echo "div($a, $b) = " . div($a, $b) . PHP_EOL;
+//echo "summ($a, $b) = " . summ($a, $b) . PHP_EOL;
+//echo "subtraction($a, $b) = " . subtraction($a, $b) . PHP_EOL;
+//echo "mul($a, $b) = " . mul($a, $b) . PHP_EOL;
+//echo "div($a, $b) = " . div($a, $b) . PHP_EOL;
+//echo "div($a, 0) = " . div($a, 0) . PHP_EOL;
---
+++ lesson2/task4.php
@@ -0,0 +1,41 @@
+<?php
+include "task3.php";
+
+$arg1 = 10;
+$arg2 = 5;
+
+
+function mathOperation($arg1, $arg2, $operation)
+{
+    switch ($operation) {
+        case "summ":
+        {
+            return summ($arg1, $arg2);
+        }
+
+        case "subtraction":
+        {
+            return subtraction($arg1, $arg2);
+        }
+
+        case "mul":
+        {
+            return mul($arg1, $arg2);
+        }
+
+        case "div":
+        {
+            return div($arg1, $arg2);
+        }
+
+        default:
+            break;
+    }
+}
+
+echo "mathOperation($arg1, $arg2, 'summ') = " . mathOperation($arg1, $arg2, "summ") . PHP_EOL;
+echo "mathOperation($arg1, $arg2, 'subtraction') = " . mathOperation($arg1, $arg2, "subtraction") . PHP_EOL;
+echo "mathOperation($arg1, $arg2, 'mul') = " . mathOperation($arg1, $arg2, "mul") . PHP_EOL;
+echo "mathOperation($arg1, $arg2, 'div') = " . mathOperation($arg1, $arg2, "div") . PHP_EOL;
+
+
